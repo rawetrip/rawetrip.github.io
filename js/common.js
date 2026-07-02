00eb72f04a5c11ece50b1e8c5c13f3e724872e0d
rawetrip <rawetrip@users.noreply.github.com>
2026-07-02 13:50:49 +0800

    fix: AsakuraKaren合并为后藤别名，移除重复页面/条目

--- js/common.js
+++ js/common.js
@@ -129,9 +129,8 @@ var _searchIndex = [
   {t:"Anjiu Ovieo", u:"bio/anjiu-ovieo.html", c:"人物", p:"anjiu ovieo", k:"安九 蔡洪浩 鸡 爱丽丝 微电影导演 铸币聚集地"},
   {t:"Makesade", u:"bio/Makesade.html", c:"人物", p:"makesade", k:"马可赛德 马克萨德 改车王 御坂美琴 学生 福建"},
   {t:"南瓜", u:"bio/nangua.html", c:"人物", p:"nangua", k:"南瓜 KOOK 铸币聚集地 Anjiu 恋人"},
-  {t:"后藤", u:"bio/goto.html", c:"人物", p:"houteng", k:"后藤 goto 铸币聚集地 兄弟"},
+  {t:"后藤", u:"bio/goto.html", c:"人物", p:"houteng", k:"后藤 goto AsakuraKaren 铸币聚集地 兄弟 闺蜜 Makesade"},
   {t:"韩堡戈", u:"bio/hanburger.html", c:"人物", p:"hanbaoge", k:"韩堡戈 Hanburger 兄弟 Anjiu CS2"},
-  {t:"AsakuraKaren", u:"bio/asakurakaren.html", c:"人物", p:"asakurakaren", k:"AsakuraKaren 后藤 闺蜜 Makesade"},
 
   // ── 事件 ──
   {t:"标记炸弹安放点 B", u:"events.html#e1", c:"事件 · Anjiu", p:"biaojizhadananfangdian b", k:"CS2炸弹B点标记 战术"},
